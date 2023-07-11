8077d67b96215ed2f4713e4071a79e7c4fdef2af
Kannan G <gkannan271120@gmail.com>
2023-07-11 20:11:26 +0530

    Added animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="h-screen flex justify-center items-center transition duration-150 ease-out hover:ease-in">
+    <div className="h-screen flex justify-center items-center  ease-out hover:ease-in">
       <Link
         to="/nasaphoto"
-        className="p-7 font-mono text-white text-5xl rounded-3xl uppercase border-4 hover:text-6xl hover:rounded-none"
+        className="p-7 font-mono text-white text-5xl transition-all duration-1000 rounded-3xl uppercase border-4 hover:text-6xl hover:rounded-none hover:transform"
       >
         See into the stars!
       </Link>
